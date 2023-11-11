33937736e85467e96b26dd8b1fa0bf1ad49384a3
Karthik Shankar <misterkar6@gmail.com>
2023-11-11 17:33:40 -0500

    authentication basics

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,15 @@
-export default function Home() {
-    return (
-        <h1>testing prifle</h1>
-    )
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  return (
+    <>
+      {user ? (
+        <a href="/api/auth/logout">Logout</a>
+      ) : (
+        <a href="/api/auth/login">Login</a>
+      )}
+    </>
+  );
 }
