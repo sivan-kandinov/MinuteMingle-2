22af915631bc7b431f10f088ebce2baf83c12d1d
Josh Daniel <joshtdaniel@gmail.com>
2023-11-11 20:34:16 -0500

    Arrow Works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import RightArrow from './components/RightArrow'
 import Navbar from '../pages/components/Navbar.jsx';
 
 type ConnectionStatus = {
@@ -36,20 +37,24 @@ export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div className="bg-gray-200">
-      <div className="mx-auto mt-10 w-1/2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a>
-        <img className="rounded-t-lg" src="/images/happy_guy.jpg" alt="" />
-    </a>
-    <div className="p-5">
-        <a href="#">
-          {/* HAVE TO REPLACE WITH THE ACTUAL PERSON'S NAME */}
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jeremy, 22</h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Interests:</p>
+    <div>
+      <div className="absolute top-1/2 right-24">
+        <RightArrow />
+      </div>
+      <div className="bg-gray-200">
+        <div className="mx-auto mt-10 w-1/2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <a>
+              <img className="rounded-t-lg" src="/images/happy_guy.jpg" alt="" />
+          </a>
+          <div className="p-5">
+              <a href="#">
+                {/* HAVE TO REPLACE WITH THE ACTUAL PERSON'S NAME */}
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jeremy, 22</h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Interests:</p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-    </div>
-    
 );
 }
