29d0485a4fffde3edd99fa41a0033f8afdc8b291
lindsayydevine <99309612+lindsayydevine@users.noreply.github.com>
2023-11-11 23:34:33 -0500

    landing page

--- pages/landing.js
+++ pages/landing.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function landing() {
     return (
       
-      <div className="m-8">
-        <h1 style={{fontSize:'100px', textAlign:'center'}}>Welcome!</h1>
-            <button type="button" id="login-button" style={{fontSize:'100px', textAlign:'center'}}> Login </button>
+      <div style={{fontSize:'100px', textAlign:'center'}}>
+        <h1>Welcome!</h1>
+        <button type="button" id="login-button"> Login </button>
       </div>
 
       
