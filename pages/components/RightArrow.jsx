0c930d2e111bf126951a18ad00c6b432c663addb
Karthik Shankar <misterkar6@gmail.com>
2023-11-12 00:18:02 -0500

    actually able to swipe on people

--- pages/components/RightArrow.jsx
+++ pages/components/RightArrow.jsx
@@ -1,9 +1,55 @@
-export default function RightArrow() {
-    return (
-        <button onClick={() => {console.log("hi")}}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" style={{ width: '4rem', height: '4rem' }}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </button>
-    );
+export default function RightArrow({ person, setPerson, setPicture }) {
+  const maleImages = [
+    "/images/people/male/student1.png",
+    "/images/people/male/student3.png",
+    "/images/people/male/student6.png",
+    "/images/people/male/student7.png",
+    "/images/people/male/student8.png",
+    "/images/people/male/student9.png",
+  ];
+  const femaleImages = [
+    "/images/people/female/student2.png",
+    "/images/people/female/student4.png",
+    "/images/people/female/student5.png",
+    "/images/people/female/student10.png",
+    "/images/people/female/student11.png",
+  ];
+
+  const swipe = async () => {
+    const res = await fetch("/api/randomUser");
+    const json = await res.json();
+    setPerson(json);
+    const profilePic = document.querySelector("#profilePic");
+    profilePic.style.transform = "translateX(100%)";
+    profilePic.style.opacity = 0;
+    setTimeout(() => {
+      profilePic.style.transform = "translateX(0%)";
+    }, 500);
+    setTimeout(() => {
+      profilePic.style.opacity = 1;
+    }, 1000);
+    console.log(person.basicInfo.gender)
+    if (person.basicInfo.gender == "Male")
+      setPicture(maleImages[Math.floor(Math.random() * maleImages.length)]);
+    else
+      setPicture(femaleImages[Math.floor(Math.random() * femaleImages.length)]);
+  };
+  return (
+    <button onClick={() => swipe()}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1}
+        stroke="currentColor"
+        style={{ width: "4rem", height: "4rem" }}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </button>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import clientPromise from '../lib/mongodb'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-import RightArrow from './components/RightArrow'
+"use client";
+
+import clientPromise from "../lib/mongodb";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import RightArrow from "./components/RightArrow";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 type ConnectionStatus = {
-  isConnected: boolean
-}
+  isConnected: boolean;
+};
 
 export const getServerSideProps: GetServerSideProps<
   ConnectionStatus
 > = async () => {
   try {
-    await clientPromise
+    await clientPromise;
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
@@ -22,39 +26,64 @@ export const getServerSideProps: GetServerSideProps<
 
     return {
       props: { isConnected: true },
-    }
+    };
   } catch (e) {
-    console.error(e)
+    console.error(e);
     return {
       props: { isConnected: false },
-    }
+    };
   }
-}
+};
 
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [person, setPerson] = useState<any | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [picture, setPicture] = useState<string>("/images/happy_guy.jpg");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await fetch("/api/randomUser");
+      const json = await res.json();
+      setPerson(json);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="relative w-full h-full">
-      <div className="absolute top-1/2 right-36">
-        <RightArrow />
+      <div className="absolute top-1/2 right-[10%]">
+        <RightArrow person={person} setPerson={setPerson} setPicture={setPicture}/>
       </div>
       <div className="bg-gray-200">
         <div className="mx-auto mt-10 w-1/2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a>
-              <img className="rounded-t-lg" src="/images/happy_guy.jpg" alt="" />
-          </a>
+          <img
+            src={picture}
+            alt="picture of a person"
+            id="profilePic"
+            className="rounded-t-lg transition duration-500 "
+          />
           <div className="p-5">
-              <a href="#">
-                {/* HAVE TO REPLACE WITH THE ACTUAL PERSON'S NAME */}
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jeremy, 22</h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Major(s):</p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Minor(s):</p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Courses:</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {person && person.basicInfo.firstName}{" "}
+              {person && person.basicInfo.lastName},{" "}
+              {person && person.basicInfo.age}
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Major(s): {person && person.academicInfo.majors}
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Minor(s): {person && person.academicInfo.minors}
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Courses: {person && person.academicInfo.classes}
+            </p>
           </div>
         </div>
       </div>
     </div>
-);
+  );
 }
