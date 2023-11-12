f2731c32fb7e1608e61c4ff2615b7684e334d1bf
Jocelyn Velazquez <jvelazquez@umass.edu>
2023-11-11 23:00:29 -0500

    Created landing page

---
+++ pages/landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function landing() {
+    return (
+      <div className="m-8">
+            <button type="button" id="login-button"> Login </button>
+      </div>
+    );
+  }
