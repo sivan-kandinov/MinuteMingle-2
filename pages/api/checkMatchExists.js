3a916abb253b43f86b9a43ca2f64a7f6bfb7bdb1
Jocelyn Velazquez <jvelazquez@umass.edu>
2023-11-11 23:42:07 -0500

    Updated checkMatchExists route

--- pages/api/checkMatchExists.js
+++ pages/api/checkMatchExists.js
@@ -5,19 +5,28 @@ export default async (req, res) => {
        const client = await clientPromise;
        const db = client.db("MinuteMingle");
        const info = req.body
+       console.log(info.sender)
        const match = await db
            .collection("Matches")
            .findOne({ sender: info.sender, receiver: info.receiver})
 
         if(match === null){
             res.status(200).send({found:false})
-        }
-
-        const contactInfo = await db.
+        }else{
+            const contactInfo = await db.
             collection("userdatas")
             .findOne({username:info.receiver})
 
-        res.status(200).send({found:true, receiverInfo:contactInfo});
+            const currentUser = await db.collection("studyBuddies").findOne({user:info.sender})
+            console.log(currentUser)
+            if(currentUser === null){
+                db.collection("studyBuddies").insertOne({user:info.sender,matches:[info.receiver]})
+            }else{
+                currentUser.matches.push(info.receiver)
+                db.collection("studyBuddies").updateOne({"user":info.sender},{$set:{"matches":currentUser.matches}})
+            }
+            res.status(200).send({found:true, receiverInfo:contactInfo});
+        }
    } catch (e) {
        console.error(e);
        res.status(400).end();
